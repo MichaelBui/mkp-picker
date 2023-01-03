8557243d50ae9d2434837267131448e93133b8bb
Michael Bui <michael.bui@fairprice.com.sg>
2023-01-03 16:49:20 +0800

    Initited demo iOS/Android apps with barcode scanning

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  data: any;
+  constructor(private barcodeScanner: BarcodeScanner, private alertController: AlertController) {}
 
-  constructor() {}
+  async scan() {
+    this.barcodeScanner.scan().then(async barcodeData => {
+      const alert = await this.alertController.create({
+        header: 'Barcode',
+        subHeader: barcodeData.format,
+        message: barcodeData.text,
+        buttons: ['OK'],
+      });
+  
+      await alert.present();
+    }).catch(async err => {
 
+      const alert = await this.alertController.create({
+        header: 'Error',
+        subHeader: 'Barcode scanning error',
+        message: err,
+        buttons: ['OK'],
+      });
+  
+      await alert.present();
+    });
+  }
 }
